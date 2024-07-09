2eb8c4da3b93ae8040715eba54f4e5e919b51947
SkyfuN23 <juli10capo@hotmail.com>
2024-07-09 03:37:18 -0300

    cambio

--- gym_backend/src/database/database.js
+++ gym_backend/src/database/database.js
@@ -1,11 +1,9 @@
-// database.js (Archivo para la conexión a MongoDB)
 const mongoose = require("mongoose");
 
 const URI = "mongodb://localhost:27017/gym";
 
-mongoose
-    .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("DB is connected"))
-    .catch((err) => console.error(err));
+mongoose.connect(URI)
+    .then(() => console.log("MongoDB connected successfully"))
+    .catch((err) => console.error("Error connecting to MongoDB:", err));
 
 module.exports = mongoose;

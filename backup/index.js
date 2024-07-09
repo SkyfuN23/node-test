34a06138082559f86d6956eda01afde27b54d9aa
SkyfuN23 <juli10capo@hotmail.com>
2024-07-09 05:09:21 -0300

    cambio

--- backup/index.js
+++ backup/index.js
@@ -1,16 +1,16 @@
-const { exec } = require('child_process');
+// const { exec } = require('child_process');
 
-const backupCommand = 'mongodump --out D:/mongodb'; //CAMBIAR LA RUTA A LA QUE SE QUIERE EXPORTAR "D:/mongodb"
+// const backupCommand = 'mongodump --out D:/mongodb'; //CAMBIAR LA RUTA A LA QUE SE QUIERE EXPORTAR "D:/mongodb"
 
-function backup() {
-    exec(backupCommand, (error, stdout, stderr) => {
-        console.log(`Respaldo completado: ${stdout}`);
-        console.log(`Backup ${new Date().toLocaleDateString()}`);
-    });
-}
+// function backup() {
+//     exec(backupCommand, (error, stdout, stderr) => {
+//         console.log(`Respaldo completado: ${stdout}`);
+//         console.log(`Backup ${new Date().toLocaleDateString()}`);
+//     });
+// }
 
-backup();
+// backup();
 
-setInterval(() => {
-    backup();
-}, 60000 * 60 * 24);
+// setInterval(() => {
+//     backup();
+// }, 60000 * 60 * 24);
--- gym/src/main.jsx
+++ gym/src/main.jsx
@@ -5,7 +5,7 @@ import { io } from 'socket.io-client';
 import Router from './Router.jsx';
 import './styles/all.css';
 
-export const URL = `http://164.92.247.255:3000`;
+export const URL = `http://46.101.200.58:3000`;
 export const socket = io(URL);
 
 ReactDOM.createRoot(document.getElementById('root')).render(

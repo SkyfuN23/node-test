d445effc3b0a89258cfc2f698811fc3cfea28b9f
SkyfuN23 <juli10capo@hotmail.com>
2024-07-09 03:33:43 -0300

    prueba database

--- gym_backend/src/database/database.js
+++ gym_backend/src/database/database.js
@@ -1,10 +1,11 @@
+// database.js (Archivo para la conexión a MongoDB)
 const mongoose = require("mongoose");
 
-const URI = "mongodb://localhost:27017/gym";;
+const URI = "mongodb://localhost:27017/gym";
 
 mongoose
-    .connect(URI)
-    .then((db) => console.log("DB is connected"))
+    .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("DB is connected"))
     .catch((err) => console.error(err));
 
 module.exports = mongoose;
--- gym_backend/src/index.js
+++ gym_backend/src/index.js
@@ -1,6 +1,7 @@
-require('./database/database.js');
+// server.js (Archivo principal para iniciar el servidor Socket.IO y la lógica de negocio)
 const { Server } = require('socket.io');
 const socketFunctions = require('./socketFunctions.js');
+const mongoose = require("./database/database.js");
 
 const io = new Server(3000, {
     cors: {
@@ -9,6 +10,9 @@ const io = new Server(3000, {
     },
 });
 
-io.on('connection', (socket) => socketFunctions(io, socket));
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    socketFunctions(io, socket);
+});
 
 console.log('Socket.io server running on port 3000');
